641557c9a1089bcfc96610c08877908449f36d8e
Guillermo Montalban Martínez <44685436+GuillermoMontalbanMartinez@users.noreply.github.com>
2020-11-19 11:27:27 +0100

    Update Ejercicio2.c

--- Guillermo/Practica02/Ejercicio2.c
+++ Guillermo/Practica02/Ejercicio2.c
@@ -1,11 +1,11 @@
 
 /****************************** EJERCICIO 02 *********************************
-**	Desarrollar un programa que busque el mínimo de un vector de valores 	**
+**	Desarrollar un programa que busque el mínimo de un vector de valores 	**
 **	reales (double) aleatorios de forma paralela. La longitud del vector	**
-**	del vector se debe indicar por parámetro. La carga de trabajo debe 		**
-**	balancearse de la forma más equilibrada posible. El procesador cero es	**
-** 	el que dispone de la información sobre el vector. El procesador dos 	**
-** 	será el encargado de recoger la información y mostrarla por pantalla.	**
+**	del vector se debe indicar por parámetro. La carga de trabajo debe 		**
+**	balancearse de la forma más equilibrada posible. El procesador cero es	**
+** 	el que dispone de la información sobre el vector. El procesador dos 	**
+** 	será el encargado de recoger la información y mostrarla por pantalla.	**
 ******************************************************************************/
 
 
@@ -17,9 +17,14 @@
 
 int main(int arg, char *argv[]) {
 	int size = 0, rank = 0;
-	int length = 0;
-	double *vector = NULL;
+	int length_vector = 0;
+	double *vector_in = NULL;
+	double *vector_out = NULL;
 	double mininum = 9999.99;
+	double elements_for_the_processor = 0.0;
+	int elements_for_the_processor_int = 0;
+
+	srand48(time(NULL));
 
 	MPI_Init(&arg, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size); 
@@ -38,17 +43,44 @@ int main(int arg, char *argv[]) {
 			fflush(stdin);
 			fflush(stdout);
 			scanf("%i", length);
-		} while (length <= 0);
+		} while (length_vector <= 0);
 
-		vector = (double*) calloc(length,sizeof(double));
-		srand48(time(NULL));
+		printf("CPU 0: \n");
+		fillVectorDouble(vector_in,length);
+		showVectorDouble(vector_in,length);
+		printf("\n");
 
-		for (int i = 0; i < length; i++) {
-			vector[i] = drand48() * (1000.0 - 1.0) + 1.0; // numRandom = dran48() * (n-m) + n;
+		if (isNumberEven(length)) {
+			for (int i = 1; i < size; i++) {
+				MPI_Send(&length_vector, 1, MPI_INT, 1, 123, MPI_COMM_WORLD);
+			}
+
+			vector_in = (double*) calloc(length_vector,sizeof(double));
+			elements_for_the_processor_int = length_vector / size;
+			vector_out = (double*) calloc(elements_for_the_processor_int, sizeof(double));
+
+			MPI_Scatter(&vector_in, elements_for_the_processor, MPI_Double, elements_for_the_processor, MPI_Double, 0, MPI_COMM_WORLD);
+
+		} else {
+			for (int i = 1; i < size; i++) {
+				MPI_Send(&length_vector, 1, MPI_INT, 1, 123, MPI_COMM_WORLD);
+			}
+
+			vector_in = (double*) calloc(length_vector,sizeof(double));
+
+			elements_for_the_processor = ((double) length_vector / (double) size);
+
+			if ((elements_for_the_processor / 0.5) == elements_for_the_processor * 2) {
+				elements_for_the_processor += 0.1;
+			}
 		}
 
 	} else if (rank == 1) {
+		MPI_Recv(&length_vector, 1, MPI_INT, 0, 123, MPI_COMM_WORLD, &status);
+		elements_for_the_processor = (double) length / (double) size;
 
+		if () 
+		MPI_Scatter(&vector_in,elements_for_the_processor,MPI_Double,elements_for_the_processor,MPI_Double,0,MPI_COMM_WORLD);
 	}
 
 
