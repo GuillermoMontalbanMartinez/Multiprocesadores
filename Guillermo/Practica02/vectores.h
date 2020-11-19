f6d058682aa1c223d2228a56414f85b7cc453c6e
Guillermo Montalban Martínez <44685436+GuillermoMontalbanMartinez@users.noreply.github.com>
2020-11-19 11:19:55 +0100

    Update vectores.h

--- Guillermo/Practica02/vectores.h
+++ Guillermo/Practica02/vectores.h
@@ -8,9 +8,11 @@
 #include <stdbool.h>
 
 void fillVector(int*vector, int n);
-void fillVectorDouble(double *vector, int n);
+void fillVectorDouble(double *vector, int size);
 void showVector(int*vector, int n);
-void showVector(double *vector, int n);
+void showVectorDouble(double *vector, int size);
 bool isNumberEven(int number_of_clients, int size);
+bool isNumberEvenV2(int length);
+double mininumVectorNumber(double *vector, int size);
 
 #endif
