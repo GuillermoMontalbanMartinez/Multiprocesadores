af7b931a312adce71df9d4f37ee64070ecff2e65
Guillermo Montalban Martínez <44685436+GuillermoMontalbanMartinez@users.noreply.github.com>
2020-12-05 16:49:04 +0100

    Version 0.1
    Comparar cadenas, quitar lineas en blanco innecesarias en el archivo

--- Guillermo/Practica02/Ejercicio06.c
+++ Guillermo/Practica02/Ejercicio06.c
@@ -2,41 +2,41 @@
 
 	Considere la siguiente lista de países y sus capitales:
 			|********************************************************************|
-			| 				España			|				Madrid				 |
+			| 			España				|				Madrid				 |
 			|********************************************************************|
-			| 				Inglaterra		|				Londres				 |
+			| 			Inglaterra			|				Londres				 |
 			|********************************************************************|
-			| 				Francia			|				París				 | 
+			| 			Francia				|				París				 | 
 			|********************************************************************|
-			| 				Alemania		|				Bonn				 |
+			| 			Alemania			|				Bonn				 |
 			|********************************************************************|
-			| 				India			|				Nueva Delhi			 |
+			| 			India				|				Nueva Delhi			 |
 			|********************************************************************|
-			| 				Israel			|				Jerusalén			 | 
+			| 			Israel				|				Jerusalén			 | 
 			|********************************************************************|
-			| 				Italia			|				Roma				 |
+			| 			Italia				|				Roma				 |
 			|********************************************************************|
-			| 				Japón			|				Tokio				 |
+			| 			Japón				|				Tokio				 |
 			|********************************************************************|
-			| 				Méjico			|				Ciudad de Méjico	 |
+			| 			Méjico				|				Ciudad de Méjico	 |
 			|********************************************************************|
-			| 				China			|				Pekín				 |
+			| 			China				|				Pekín				 |
 			|********************************************************************|
-			| 				Rusi			|				Moscú				 | 
+			| 			Rusi				|				Moscú				 | 
 			|********************************************************************|
-			| 				Estados Unidos	|				Washington			 |
+			| 			Estados Unidos		|				Washington			 |
 			|********************************************************************|
-			| 				Canadá			|				Ottawa				 | 
+			| 			Canadá				|				Ottawa				 | 
 			|********************************************************************|
 
-	Escriba un programa interactivo que acepte el nombre de un país como entrada
+	Escriba un programa interactivo que acepte el nombre de un país como entrada
 	y escriba su correspondiente capital y viceversa en el menor tiempo posible.
-	Diseñe el programa de modo que se ejecute repetidamente hasta que se introduzca
-	la palabra Fin. Para el diseño del programa tenga en cuenta que el procesador
+	Diseñe el programa de modo que se ejecute repetidamente hasta que se introduzca
+	la palabra Fin. Para el diseño del programa tenga en cuenta que el procesador
 	central (aquel con identificador cero) es el que solicita al usuario el nombre
-	del país de entrada con el usuario. Además, será también el que conoce los 
+	del país de entrada con el usuario. Además, será también el que conoce los 
 	datos y, por tanto, el encargado de distribuir la carga. El resultado final
-	lo mostrará el procesador con identificador 1.
+	lo mostrará el procesador con identificador 1.
  */
 
 #include <stdio.h>
@@ -77,34 +77,27 @@ int main(int argc, char argv[]) {
 
 		} else if (rank == 1) {
 			MPI_Recv(&nombre_pais,15,MPI_INT,0,123,MPI_COMM_WORLD,&status);
+
+			size_t len = strlen(nombre_pais); // Longitud de una cadena de texto
+        	nombre_pais[len - 1] = '\0';
+
+        	for (int i=0; i < N; i++) {
+        		if (strstr(paises[i].nombre_pais, nombre_pais)) {
+            		printf("\nEl nombre de la capital del pais %s es %s\n", nombre_pais, paises[i].capital_del_pais);
+            	}
+       		 }
 		} else {
 			MPI_Recv(&nombre_pais,15,MPI_INT,0,123,MPI_COMM_WORLD,&status);
+			size_t len = strlen(nombre_pais); // Longitud de una cadena de texto
+        	nombre_pais[len - 1] = '\0';
+
+        	for (int i=0; i < N; i++) {
+        		if (strstr(paises[i].nombre_pais, nombre_pais)) {
+            	printf("\nEl nombre de la capital del pais %s es %s\n", nombre_pais, paises[i].capital_del_pais);
+            	}
+       		}
 		}
 	} while (!strstr(nombre_pais,"Fin"));
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
