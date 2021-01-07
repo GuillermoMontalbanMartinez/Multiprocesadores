5af52b0a6f115c8f0930e52a7f68436bee31466d
Guillermo Montalban Martínez <44685436+GuillermoMontalbanMartinez@users.noreply.github.com>
2021-01-07 20:09:03 +0100

    Operaciones con matrices ficheros

---
+++ Proyecto/Matrices.h
@@ -0,0 +1,6 @@
+#ifndef MATRICES_PROYECTO_FINAL
+#define MATRICES_PROYECTO_FINAL
+	void rellenarMatrizAleatoriaDoubles(double** matriz, int row, int column);
+	void mostrarMatriz(double** matriz, int row, int column);	
+	double** reservarMatriz(int row, int column);
+#endif
