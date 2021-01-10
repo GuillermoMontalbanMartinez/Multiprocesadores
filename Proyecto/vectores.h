70ea4e3863b4a6b3d5c484f6ab377043eae7aa97
Guillermo Montalban Martínez <44685436+GuillermoMontalbanMartinez@users.noreply.github.com>
2021-01-10 16:27:34 +0100

    Delete vectores.h

--- Proyecto/vectores.h
+++
@@ -1,9 +0,0 @@
-#ifndef MATRICES_PROYECTO_FINAL
-#define MATRICES_PROYECTO_FINAL
-    double** reservarMatriz(int fila, int columna);
-    void liberarMatriz(double** matriz);
-    void llenarMatrizAleatoria(int fila, int columna, double** matriz, int min, int max);
-    void mostrarMatriz(int fila, int columna, double** matriz);
-    double buscarMinimoMatriz(int fila, int columna, double** matriz);
-    void multiplicarMatrices(double** matriz_c, int filas_matriz_a, int columnas_matriz_b, double** matriz_a, int columnas_matriz_a, double** matriz_b, int filas_matriz_B);
-#endif
